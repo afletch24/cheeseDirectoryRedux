74da4b96b578b7208c5e501a445268e7ecf19929
Audrey <anfletcher24@gmail.com>
2018-06-07 13:44:39 -0500

    transitioning to redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,29 +1,43 @@
 import React, { Component} from 'react';
+import { connect } from 'react-redux';
+import { setSearchField } from "../actions";
+
 import CardList from '../components/CardList';
-import {cheese} from '../cheese';
+import { cheese } from '../cheese';
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import './App.css';
 import ErrorBoundry from "../components/ErrorBoundry";
 
+
+
+const mapStateToProps = state => {
+    
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
 class App extends Component{
     constructor(){
         super()
         this.state = {
-            searchfield: ''
+            cheese: cheese,
         }
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-        
-    }
-
    render(){
+        const { cheese } = this.state;
+        const { searchField, onSearchChange} = this.props;
         const filteredCheese = cheese.filter(c => {
-            
             const currentName = c.name.toLowerCase()
-            const includesSearchTerm = currentName.includes(this.state.searchfield.toLowerCase())
+            const includesSearchTerm = currentName.includes(searchField.toLowerCase())
             return includesSearchTerm
             // return c.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
@@ -34,15 +48,16 @@ class App extends Component{
         return(
             <div className="background">
                 <h1> Cheese </h1>
-                <SearchBox searchChange = {this.onSearchChange}/>
+                <SearchBox searchChange = {onSearchChange}/>
                 <Scroll>
                     <ErrorBoundry>
                         <CardList fcheese={ filteredCheese } />
                     </ErrorBoundry>
                 </Scroll>
             </div>
-    ) 
+    );
    }  
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import './index.css';
 import './fonts.css';
-
+import { searchCheese } from "./reducers";
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(searchCheese);
+
+
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 
 
